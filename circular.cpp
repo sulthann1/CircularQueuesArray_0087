e337f91b5687283fd4b03a996f89117194ee504b
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-12 18:32:02 +0700

    jika FRONT_position > REAR_position, iterasi dadri FRONT hingga akhir array

--- circular.cpp
+++ circular.cpp
@@ -98,7 +98,12 @@ class Queues
     }
     else
     {
-        
+        //jika FRONT_position > REAR_position, iterasi dadri FRONT hingga akhir array
+        while (FRONT_position <= max -1)
+        {
+            cout << queue_array[FRONT_position]<< " ";
+            FRONT_position++;
+        }
     }
 
     }
