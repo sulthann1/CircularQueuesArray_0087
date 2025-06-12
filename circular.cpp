9a377b217ec88364ceefe83c4e1f1c8486f1a88a
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-12 17:44:18 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- circular.cpp
+++ circular.cpp
@@ -36,6 +36,14 @@ class Queues
             REAR = 0; //2.b
         }
         else
+        {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+            REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 
 
