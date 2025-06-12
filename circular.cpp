905d4417afa449b05b360a86887b56fd281cf151
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-12 18:36:27 +0700

    menambahkan int main

--- circular.cpp
+++ circular.cpp
@@ -110,9 +110,20 @@ public:
             while (FRONT_position <= REAR_position)
             {
                 cout << queue_array[FRONT_position] << " ";
-                FRONT_position
+                FRONT_position;
             }
             cout << endl;
         }
     }
 };
+
+int main ()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        
+    }
+}
