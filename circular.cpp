4c12e5a86d6d7539c7ac11a34e8bc08df5105249
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-12 17:48:41 +0700

    cek apakah antrian memiliki satu elemen

--- circular.cpp
+++ circular.cpp
@@ -51,8 +51,18 @@ class Queues
         //cek apakah antrian kosong
         if (FRONT == -1)
         {
-            cout 
+            cout << "Queue underflow\n";
+            return;
         }
+        cout << "\nThe element deleted from the queue is :" << queue_array [FRONT] << "\n";
+
+        //cek apakah antrian memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1 ;
+            REAR = -1;
+        }
+        
     }
 
 
