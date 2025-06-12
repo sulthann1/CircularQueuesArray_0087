94161745246dbdc4c0dd8215b24ebf1c80354c9a
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-12 17:40:20 +0700

    menegecek apakah antrian penuh

--- circular.cpp
+++ circular.cpp
@@ -19,7 +19,16 @@ class Queues
     {
         int num;
         cout << "Enter a number :";
+        cin >> num;
+        cout << endl;
+
+        //1. cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; //1.a
+            return ;                     //1.b
+        }
     }
 
 
-}
+};
